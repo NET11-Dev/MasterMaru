998868b0bf779c52560dc70f296ff3485836bb68
Abap <abap@net11.cl>
2019-10-29 09:37:58 -0300

    Version 1.0
    Carga Fuente Original A Git

---
+++ Caja Recaudadora/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Caja_Recaudadora.MvcApplication" Language="C#" %>
---
+++ Caja Recaudadora/ServiciosRfc/Rfc_AperturaCierre/CAJA_LOGIN.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Caja_Recaudadora.ServiciosRfc.Rfc_AperturaCierre
+{
+    public class CAJA_LOGIN
+    {
+
+    }
+}
